f432d8df213b0364183c0b23ca6118ef701701a1
Gary Zhu <390992476@qq.com>
2020-05-10 18:02:57 +0800

    update package to 1.2.0

--- src/LeopardToolKit.AspNetCore/Cookies/ICookieManager.cs
+++ src/LeopardToolKit.AspNetCore/Cookies/ICookieManager.cs
@@ -14,18 +14,25 @@ namespace LeopardToolKit.AspNetCore.Cookies
 
     public static class CookieManagerExtension
     {
-        public static void SetCookie(this ICookieManager cookieManager, string key, string value, DateTimeOffset? expires = null)
+        public static void SetCookie(this ICookieManager cookieManager, string key, string value, DateTimeOffset? expires)
         {
             CookieOptions cookieOptions = new CookieOptions();
             cookieOptions.Expires = expires;
             cookieManager.SetCookie(key, value, cookieOptions);
         }
 
-        public static void SetCookie(this ICookieManager cookieManager, string key, string value, TimeSpan? maxAge = null)
+        public static void SetCookie(this ICookieManager cookieManager, string key, string value, TimeSpan? maxAge)
         {
             CookieOptions cookieOptions = new CookieOptions();
             cookieOptions.MaxAge = maxAge;
             cookieManager.SetCookie(key, value, cookieOptions);
         }
+
+        public static void SetCookie(this ICookieManager cookieManager, string key, string value)
+        {
+            CookieOptions cookieOptions = new CookieOptions();
+            cookieManager.SetCookie(key, value, cookieOptions);
+        }
+
     }
 }
