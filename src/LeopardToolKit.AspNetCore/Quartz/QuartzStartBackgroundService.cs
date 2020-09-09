25981c4de49bc6a70fb5b72e26831852ada20201
Gary Zhu <ZHUGX@legal.regn.net>
2020-09-09 09:08:52 +0800

    add Quartz and update version 1.3.0

---
+++ src/LeopardToolKit.AspNetCore/Quartz/QuartzStartBackgroundService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz.NetCore.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LeopardToolKit.AspNetCore.Quartz
+{
+    internal class QuartzStartBackgroundService : IHostedService
+    {
+        private readonly QuartzLifeTimeManager quartzLifeTimeManager;
+
+        public QuartzStartBackgroundService(QuartzLifeTimeManager quartzLifeTimeManager)
+        {
+            this.quartzLifeTimeManager = quartzLifeTimeManager;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await this.quartzLifeTimeManager.Start();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await this.quartzLifeTimeManager.Stop(true);
+        }
+    }
+}
